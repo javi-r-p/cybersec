bcbf0c9033b5549e9f2a97717e7b32ed64ea4717
javi-r-p <7460103@alu.murciaeduca.es>
2025-03-07 13:48:31 +0100

    Script developing continued

--- iptablesm.sh
+++ iptablesm.sh
@@ -1,67 +1,100 @@
 #!/bin/bash
 clear
 
-# Gestión de reglas de IPTables
-echo "Gestor de reglas IPTables"
+# Rules managing for IPTables
+echo "IPTables rules manager"
 
-# Definición de colores
-fincolor='\e[0m'	    	        #Eliminar color
-negritaamarillo='\033[1;33m'        #Amarillo negrita
-negritacian='\033[1;36m'            #Cián negrita
-amarillo='\033[0;93m'               #Amarillo intenso
-rojo='\033[0;91m'                   #Rojo intenso
-verde='\033[0;92m'                  #Verde intenso
-azul='\033[0;94m'                   #Azul intenso
-cian='\033[0;96m'                   #Cián intenso
-negritacianintenso='\033[1;96m'     #Cián intenso y negrita
-fondorojo='\033[0;101m'             #Fondo rojo e intenso
+# Colours definition
+nocolour='\e[0m'                #Delete colour
+boldyellow='\033[1;33m'         #Bold yellow
+boldcyan='\033[1;36m'           #Bold cyan
+yellow='\033[0;93m'             #Bright yellow
+red='\033[0;91m'                #Bright red
+green='\033[0;92m'              #Bright green
+blue='\033[0;94m'               #Bright blue
+cyan='\033[0;96m'               #Bright cyan
+brightboldcyan='\033[1;96m'     #Bold and bright cyan
+backred='\033[0;101m'           #Bright red background
 
-# Función de salida con control de errores
-function salida() {
-    codigo=$1
-    if [ $codigo -eq 0 ]; then
-        echo -e "${verde}Programa finalizado ($codigo)${fincolor}";
-        exit $codigo;
+# Error control and exit function
+function quit() {
+    code=$1
+    if [ $code -eq 0 ]; then
+        echo -e "${green}Program has ended with code $code${nocolour}";
+        exit $code;
     else
-        echo -e "${rojo}Programa finalizado ($codigo)${fincolor}";
-        exit $codigo
+        echo -e "${red}Programa has ended with code $code${nocolour}";
+        exit $code;
     fi
 }
 
-# Comprobar que quien ejecuta el script es el usuario root
-if [ $EUID != 0 ]; then
-    echo "El usuario que ejecute este script debe ser el usuario root";
-    salida 1;
+# Check the running user is root
+if [ $EUID -ne 0 ]; then
+    echo "You must be root to run this script";
+    quit 1;
 fi
 
-function crear() {
-    tabla=$1
-    echo -e "${verde}Crear una regla${fincolor}";
+# Function to add a rule
+function add() {
+    table=$1;
+    echo -e "${green}Add a rule${nocolour}";
+    if [ $table -eq 1 ]; then
+        echo "The rule will be added to the filter table";
+        read -p "Select a chain to which add the rule: (INPUT / OUTPUT / FORWARD) " chain;
+        if [ $chain == "INPUT" ]; then
+            echo "The rule will be added to the INPUT chain";
+        elif [ $chain == "OUTPUT" ]; then
+            echo "The rule will be added to the OUTPUT chain";
+        elif [ $chain == "FORWARD" ]; then
+            echo "The rule will be added to the FORWARD chain";
+        else
+            echo "The chain you have chosen doesn't exist";
+            quit 1;
+        fi
+    else
+        echo "The rule will be added to the NAT table";
+    fi
 }
-function listar() {
-    tabla=$1
-    echo -e "${azul}Ver listado de reglas${fincolor}";
+
+# Function to view rules
+function list() {
+    table=$1;
+    echo -e "${blue}View rules${nocolour}";
+    echo "Choose a table:";
+    echo "1. NAT";
+    echo "2. Filter";
+    echo "3. All";
+
 }
-function eliminar {
-    echo -e "${fondorojo}Eliminar una regla${fincolor}";
+
+# Function to delete a rule
+function delete {
+    echo -e "${backred}Delete a rule${nocolour}";
 }
+
+# Menu
 echo "-----";
-echo "Opciones:";
-echo -e "${verde}1. Crear una regla${fincolor}";
-echo -e "${azul}2. Ver listado de reglas${fincolor}";
-echo -e "${fondorojo}3. Eliminar una regla${fincolor}";
-echo -e "${amarillo}4. Salir${fincolor}";
-read -p "Selecciona una opcion: " opcion;
-case $opcion in
+echo "Available options:";
+echo -e "${green}1. Add a rule${nocolour}";
+echo -e "${blue}2. View rules${nocolour}";
+echo -e "${backred}3. Delete a rule${nocolour}";
+echo -e "${yellow}4. Exit${nocolour}";
+read -p "Choose an action: " option;
+case $option in
     1)
-        echo "1. Tabla NAT";
-        echo "2. Tabla filter (cortafuegos)";
-        read -p "Selecciona una tabla: " tabla;
-        crear $tabla;;
+        echo "You can add a rule in these two tables:";
+        echo "1. Filter (firewall)";
+        echo "2. NAT";
+        read -p "Choose a table: " table;
+        if [ -z $table || $table -ne 1 || $table -ne 2 ]; then
+            echo "The table you have chosen doesn't exist";
+            quit 1;
+        fi
+            add $table;;
     2)
-        listar $tabla;;
+        list $table;;
     3)
-        eliminar;;
+        delete;;
     4)
-        salida 0;;
+        quit 0;;
 esac
